10814ddb928f963cb8526cbbf9257113d3e1427c
JudithBLinder <judithrooz@gmail.com>
2020-07-01 18:51:49 -0400

    not done

--- db.js
+++ db.js
@@ -1,8 +1,10 @@
 const Sequelize = require('sequelize');
+const { Person } = require('./models');
 
 const DATABASE_URL = 'postgres://localhost:5432/sequelize_stretch_3';
 
 const db = new Sequelize(DATABASE_URL, {
   logging: false,
 });
+
 module.exports = db;
--- models/index.js
+++ models/index.js
@@ -2,4 +2,7 @@ const Person = require('./person');
 const Recipe = require('./recipe');
 //YOUR CODE HERE
 
+Recipe.belongsToMany(Person, {through: });
+Person.belongsToMany(Recipe, {through: });
+
 module.exports = { Person, Recipe };
